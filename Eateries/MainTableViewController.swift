a3ed0863350844595d115c84fec46cbe39e16d10
Novoselov Sergey <s.m.novoselov@icloud.ru>
2019-05-27 19:33:42 +0300

    App icon Launch screen Row height

--- Eateries/MainTableViewController.swift
+++ Eateries/MainTableViewController.swift
@@ -29,8 +29,16 @@ class MainTableViewController: UITableViewController {
         
         cell.textLabel?.text = reustrantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: reustrantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
 
         return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
 
     /*
