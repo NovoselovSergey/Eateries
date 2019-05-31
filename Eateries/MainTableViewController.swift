b12bbdf575afe9fc51f54b9dac5c09fc47e1a990
Novoselov Sergey <s.m.novoselov@icloud.ru>
2019-05-31 19:36:48 +0300

    New Place TableViewController

--- Eateries/MainTableViewController.swift
+++ Eateries/MainTableViewController.swift
@@ -45,5 +45,7 @@ class MainTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func cancelAction(_ segue: UIStoryboardSegue) {}
 
 }
