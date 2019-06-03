653d51ddf29748f3b79cb06481fcf3fdff0dc31f
Novoselov Sergey <s.m.novoselov@icloud.ru>
2019-06-03 23:29:42 +0300

    NewPlaceTableViewController

---
+++ Eateries/NewPlaceTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NewPlaceTableViewController.swift
+//  Eateries
+//
+//  Created by Sergey Novoselov on 03/06/2019.
+//  Copyright © 2019 Sergey Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class NewPlaceTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+    }
+    
+    //MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+}
+
+//MARK: - Text field delegate
+
+extension NewPlaceTableViewController: UITextFieldDelegate {
+    
+    // Скрываем клавиатуру по нажатию Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
